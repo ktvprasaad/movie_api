e566861cd7dc7460563c0dec4c453adb8e06b2ac
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2019-10-23 10:57:50 -0400

    Update index.js
    Updated with uuid package  and various requests (GET, POST, PUT & DELETE).

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
-const express = require('express'),
-    morgan = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 let top10Movies = [
     {
-        title: 'Avengers - Endgame',
-        director: 'Joe Russo, Anthony Russo'
+        title: 'Avengers Endgame',
+        director: 'Joe Russo, Anthony Russo',
+        genre: 'Fantasy',
+        imageUrl: 'img/AvengersEndGame.jpg'
     },
     {
         title: 'Toy Story 4',
-        director: 'Josh Cooley'
+        director: 'Josh Cooley',
+        genre: 'Fantasy',
+        imageUrl: 'img/ToyStory4.jpg'
     },
     {
         title: 'Too late to die young',
-        director: 'Dominga Sotomayor Castillo'
+        director: 'Dominga Sotomayor Castillo',
+        genre: 'Drama',
+        imageUrl: 'img/TLTDY.jpg'
     },
     {
         title: 'Hotel Mumbai',
-        director: 'Anthony Maras'
+        director: 'Anthony Maras',
+        genre: 'History',
+        imageUrl: 'img/HotelMumbai.jpg'
     },
     {
-        title: 'Captain Marvel',
-        director: 'Anna Boden, Ryan Fleck'
+        title: 'Captain-Marvel',
+        director: 'Anna Boden, Ryan Fleck',
+        genre: 'Scifi',
+        imageUrl: 'img/CaptainMarvel.jpg'
     },
-    {
-        title: 'Everybody knows',
-        director: 'Asghar Farhadi'
-    },
-    {
-        title: 'Good Boys',
-        director: 'Gene Stupnisky'
-    },
-    {
-        title: 'Photograph',
-        director: 'Ritesh Batra'
-    },
-    {
-        title: 'Dora',
-        director: 'James Bobin'
-    },
-    {
-        title: 'The Lego Movie 2',
-        director: 'Mike Mitchell'
-    }
 ]
 
 //To send all the static files - html,css,images,javascript
@@ -50,6 +45,7 @@ app.use(express.static('public'));
 //To log requests with timestamp, method, url, status and string length
 app.use(morgan('common'));
 
+//GET requests
 app.get('/',function (req, res) {
     res.send('Welcome to WebFlix Movies Online!')
 });
@@ -57,6 +53,42 @@ app.get('/',function (req, res) {
 app.get('/movies', function (req, res) {
     res.json(top10Movies)
 });
+
+app.get('/movies/:title', function (req, res) {
+    res.send('Successful GET request returning a specific movie based on title.');
+});
+
+app.get('/genre/:genre', function (req, res) {
+    res.send('Successful GET request returning genre\'s description.');
+});
+
+app.get('/director/:director', function (req, res) {
+    res.send('Successful GET request returning director\'s bio.');
+});
+
+//POST requests
+app.post('/users/:userName', function (req, res) {
+    res.send('Successful POST request creating a new user.');
+});
+
+app.post('/users/:userName/:movie', function (req, res) {
+    res.send('Successful POST request creating a favorite movie list for the  user.');
+});
+
+//PUT requests
+app.put('/users/:userName/:password/:emailId/:dob', function (req, res) {
+    res.send('Successful PUT request updating user personal details.');
+});
+
+//DELETE requests
+app.delete('/users/:userName/:movie', function (req, res) {
+    res.send('Successful DELETE request removing the specific movie from the user\'s favorite list.');
+});
+
+app.delete('/users/:userName', function (req, res) {
+    res.send('Successful DELETE request removing the user from WebFlix Movie App.');
+});
+
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
