63f3a4ecb18a2903ee8fda9afec7a5bcb6d3bf97
ktvprasaad <prasaad.vijay@gmail.com>
2019-11-15 22:04:10 -0500

    Mongoose connect update

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ app.use(cors());
 var auth=require('./auth')(app);
 var allowedOrigins = ['http://localhost:8080','http://testsite.com'];
 
-mongoose.connect('mongodb://127.0.0.1/WebFlixDB', {useNewUrlParser: true});
+// mongoose.connect('mongodb://127.0.0.1/WebFlixDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://webFlixDBadmin:Hyperb0l@@cluster0-3axny.mongodb.net/test?retryWrites=true&w=majority',
+	{useNewUrlParser: true});
+
 
 // Content-Security-Policy: default-src 'self'
 
